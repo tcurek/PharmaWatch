ed3bc0cb407cc7a21473dbb4ab975ec6af12b08d
tcurek <timcurek@gmail.com>
2019-12-16 23:01:07 -0500

    Updated scan files

--- ScanFiles.py
+++ ScanFiles.py
@@ -2,23 +2,19 @@ import json
 import os
 
 def read_file(path):
-    with open(path, 'r') as json_file:
+    with open(path, 'r', encoding='utf8', errors='ignore') as json_file:
         data = json.load(json_file)
 
     study = data['FullStudy']['Study']
     protocol_selection = study['ProtocolSection']
     identification_module = protocol_selection['IdentificationModule']
+    oversight =  study.get('OversightModule',{'OversightHasDMC':'No','IsFDARegulatedDrug':'No','IsFDARegulatedDevice':'No'})
 
-    if 'OversightModule' not in study:
-        return
-
-    oversight =  study['OversightModule']
-    
     # data
     nctId = identification_module['NCTId']
     orgStudyId = identification_module['OrgStudyIdInfo']['OrgStudyId']
-    orgFullName = protocol_selection['Organization']['OrgFullName']
-    overallStatus = study['StatusModule']['OverallStatus']
+    orgFullName = protocol_selection.get('Organization', {'OrgFullName': 'N/A'})['OrgFullName']
+    overallStatus = study.get('StatusModule', {'OverallStatus':'N/A'})['OverallStatus']
     startDate = study['StartDateStruct']['StartDate']
     primaryCompletionDate = study['PrimaryCompletionDateStruct']['PrimaryCompletionDate']
     completionDate = study['CompletionDateStruct']['CompletionDate']
@@ -27,11 +23,12 @@ def read_file(path):
     phase = study['DesignModule']['PhaseList']['Phase']
     sponsor = study['SponsorCollaboratorsModule']['LeadSponsor']['LeadSponsorName']
 
-    if (isFDARegulatedDevice == 'Yes' or isFDARegulatedDrug == 'Yes'):
-        print(nctId + ' | ' + orgStudyId  + ' | ' + orgFullName + ' | ' + primaryCompletionDate) 
+    print(path + ' | ' + nctId + ' | ' + orgStudyId  + ' | ' + orgFullName + ' | ' + primaryCompletionDate + ' | ' + isFDARegulatedDevice + ' | ' +  isFDARegulatedDrug) 
 
 
 path = 'C:\\Users\\tcurek\Desktop\\FDA'
+
+number_of_files = 0
 
 files = []
 # r=root, d=directories, f = files
@@ -39,6 +36,12 @@ for r, d, f in os.walk(path):
     for file in f:
         if '.json' in file:
             files.append(os.path.join(r, file))
+            number_of_files += 1
 
-for f in files[:10]:
+print('Starting...')
+print('Found ' + str(number_of_files) + ' files.')
+
+for f in files:
     read_file(f)
+
+print('Finished...')
